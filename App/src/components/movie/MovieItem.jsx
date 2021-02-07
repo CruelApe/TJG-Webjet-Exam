0ad39d64089e1061a0ba5c11f350dabe8699eff1
Tyron James Gono <tyron.gono@hotmail.com>
2021-02-07 17:36:11 +1100

    Updated the modal popup for movie details

--- App/src/components/movie/MovieItem.jsx
+++ App/src/components/movie/MovieItem.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import '../../views/styles/MovieItem.css'
-import { Card, Modal, Button } from 'react-bootstrap'
+import { Card, Modal, Button, Container, Row, Col } from 'react-bootstrap'
 import apiMovieService from '../../services/api/MovieApiService';
 import apiMoviesService from '../../services/api/MoviesApiService';
 
@@ -15,7 +15,6 @@ function MovieItem() {
     }, []);
 
     const handleModalClose = () => setIsModalOpen(false);
-    //const handleModalOpen = () => setIsModalOpen(true);
 
     const spinnerStyle = {
         width: '4rem',
@@ -29,11 +28,18 @@ function MovieItem() {
     };
 
     const viewMovieDetail = (event) => {
+        
+        // Reset the movie details.
+        setMovieDetails(null);
+
+        // Opens the modal
+        setIsModalOpen(true);
+
+        // Retrieves the data from the API service.
         apiMovieService
             .getSingle(movies[parseInt(event.currentTarget.accessKey)].ID)
             .then((response) => {
                 setMovieDetails(response); 
-                setIsModalOpen(true); 
             });
     };
 
@@ -66,20 +72,94 @@ function MovieItem() {
                             </div>
                         </div>
             }
-            {
-                movieDetails
-                    ?
-                        <Modal 
-                            show={isModalOpen}                                   
-                            onHide={handleModalClose} 
-                            animation={true}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>{movieDetails.Title}</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                        </Modal>
-                    : ''                 
-            }                     
+            <Modal
+                className="moviedetail-modal"
+                show={isModalOpen}                           
+                onHide={handleModalClose} 
+                animation={true}>
+                <Modal.Body>
+                {
+                    movieDetails
+                        ?
+                            <Container>
+                                <Row>
+                                    <Col xs={12} md={4}>
+                                        <Container>
+                                            <Row>
+                                                <Col>
+                                                    <div>
+                                                        <img className="movie-detail-poster" src={movieDetails.Poster} onError={handleImgError} />
+                                                    </div>                                                
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <Container>
+                                                        <Row>
+                                                            <Col xs={2} md={2}>
+                                                                <p className="movie-detail-metascore-label">METASCORE</p>
+                                                                <div className="movie-detail-metascore">{movieDetails.Metascore}</div>
+                                                            </Col>  
+                                                            <Col xs={2} md={2}>
+                                                                <p className="movie-detail-rating-label">RATING</p>
+                                                                <div className="movie-detail-rating">{movieDetails.Rating}</div>
+                                                            </Col>                                                                                                        
+                                                        </Row>
+                                                    </Container>                                                
+                                                </Col>
+                                            </Row>                                            
+                                        </Container>
+                                    </Col>
+                                    <Col xs={6} md={8}>
+                                        <h3 className="movie-detail-title">{movieDetails.Title}</h3>
+                                        <p className="movie-detail-year">{movieDetails.Year}</p>
+                                        <Container>
+                                            <Row>
+                                                <Col xs={2} md={2}>
+                                                    <p className="movie-detail-mediainfo-runtime">{movieDetails.Runtime}</p>
+                                                </Col>
+                                                <Col xs={2} md={2}>
+                                                    <div className="movie-detail-mediainfo-rated">{movieDetails.Rated}</div>
+                                                </Col>  
+                                                <Col xs={2} md={2}>
+                                                    <div className="movie-detail-mediainfo-language">{movieDetails.Language}</div>
+                                                </Col>    
+                                                <Col xs={2} md={2}>
+                                                    <div className="movie-detail-mediainfo-country">{movieDetails.Country}</div>
+                                                </Col>                                             
+                                            </Row>
+                                        </Container>
+                                        <p className="movie-detail-defaultfont">{movieDetails.Genre}</p>
+                                        <p className="movie-detail-defaultfont">{movieDetails.Plot}</p>
+                                        <p className="movie-detail-defaultfont"><b style={{fontWeight:'700'}}>Writer:</b> {movieDetails.Writer}</p>
+                                        <p className="movie-detail-defaultfont"><b style={{fontWeight:'700'}}>Director:</b> {movieDetails.Director}</p>
+                                        <p className="movie-detail-defaultfont"><b style={{fontWeight:'700'}}>Casts:</b> {movieDetails.Actors}</p>
+                                        <p className="movie-detail-defaultfont"><b style={{fontWeight:'700'}}>Awards:</b> {movieDetails.Awards}</p> 
+                                        <p className="movie-detail-defaultfont"><b style={{fontWeight:'700'}}>Votes:</b> {movieDetails.Votes}</p> 
+                                        <p className="movie-detail-price">
+                                        {
+                                            parseFloat(movieDetails.Price)
+                                            .toLocaleString('en-US', {
+                                                style: 'currency',
+                                                currency: 'USD',
+                                            })
+                                        }
+                                        </p>                                     
+                                    </Col>
+                                </Row>
+                            </Container>
+                        : 
+                        <div className="text-center">
+                            <div className="spinner-border text-info" role="status">
+                                <span className="sr-only">Loading...</span>
+                            </div>
+                        </div>                 
+                }
+                    <Modal.Footer>
+                        <Button onClick={handleModalClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal.Body>
+            </Modal>                                 
         </div>
     );
 
